9daf9ca4ba454f662b907d8504a10bb1bfa53861
Dilara Toğaçar <67376576+dilaratgcr@users.noreply.github.com>
2022-01-11 12:36:44 +0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello node");
-/*      "scripts": {
-    "start": " node index"  npm start
-  },
-  
-  
-   "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"  node index.js
-  },*/
